e53053b5a3b2eeb983ae5a5752af2db031cc734c
igorgoiis <ig_orgabriel@hotmail.com>
2021-11-04 12:26:17 -0300

    Teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 
 export default function Home() {
-  const blaa = 'var';
+  const bla = 'var';
   return (
     <div className="container">
       <Head>
