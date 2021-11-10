e76636a3ff5b42ffa5ed904c54c3695f80893524
Igor Gois <ig_orgabriel@hotmail.com>
2021-11-09 22:12:05 -0300

    Configuraçao do PWA e conclusao do Boilerplate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,8 +8,10 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>NextJS - Boilerprate</title>
-        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-        <link rel="apple-touch-icon" href="favicon.png" />
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#06092B" />
         <meta
           name="description"
           content="A simple project starter to work with TypeScript, React, NextJS and
